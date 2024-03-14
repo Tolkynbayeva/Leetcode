74b3c681c05cb2b52e8dab5e51c5204b10929925
Tolkynbayeva <workfortime37@gmail.com>
2024-03-14 20:43:23 +0500

    151. Reverse Words in a String

---
+++ reverseWordsMedium.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func reverseWords(s string) string {
+	runes := []rune(s)
+	result := ""
+
+	sentence := strings.Fields(string(runes))
+	for _, words := range sentence {
+		result = words + " " + result
+	}
+	result = strings.TrimSpace(result)
+	return result
+}
+
+func main() {
+	fmt.Println(reverseWords("the sky is blue"))  // "blue is sky the"
+	fmt.Println(reverseWords("  hello world  "))  // "world hello"
+	fmt.Println(reverseWords("a good   example")) // "example good a"
+}
