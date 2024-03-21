5a11d395e984157eefd30ae79313916e639562e4
Tolkynbayeva <workfortime37@gmail.com>
2024-03-22 02:44:41 +0500

    242. Valid Anagram

---
+++ Easy/isAnagram.go
@@ -0,0 +1,23 @@
+package main
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	var arr [26]int
+	for i := 0; i < len(s); i++ {
+		arr[s[i]-'a']++
+		arr[t[i]-'a']--
+	}
+	for _, v := range arr {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// func main() {
+// 	fmt.Println(isAnagram("anagram", "nagaram")) // true
+// 	fmt.Println(isAnagram("rat", "car")) // false
+// }
