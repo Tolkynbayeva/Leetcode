6f4a9d3ab0f0915b6ba10df5f31b7e454723713f
Tolkynbayeva <workfortime37@gmail.com>
2024-03-22 02:26:18 +0500

    217. Contains Duplicate

--- Easy/truncateSentence.go
+++ Easy/truncateSentence.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 )
 
 func truncateSentence(s string, k int) string {
-    result := ""
+	result := ""
 	array := strings.Fields(s)
 
 	for i, v := range array {
@@ -20,9 +19,8 @@ func truncateSentence(s string, k int) string {
 	return result
 }
 
-
-func main() {
-	fmt.Println(truncateSentence("Hello how are you Contestant", 4)) //"Hello how are you"
-	fmt.Println(truncateSentence("What is the solution to this problem", 4)) //"What is the solution"
-	fmt.Println(truncateSentence("chopper is not a tanuki", 5)) //"chopper is not a tanuki"
-}
+// func main() {
+// 	fmt.Println(truncateSentence("Hello how are you Contestant", 4)) //"Hello how are you"
+// 	fmt.Println(truncateSentence("What is the solution to this problem", 4)) //"What is the solution"
+// 	fmt.Println(truncateSentence("chopper is not a tanuki", 5)) //"chopper is not a tanuki"
+// }
