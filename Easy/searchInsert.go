fa0b5690fb9dd0a41e32f2f46f58525e1d2f245c
Tolkynbayeva <workfortime37@gmail.com>
2024-04-04 21:59:54 +0500

    35. Search Insert Position

--- Easy/searchInsert.go
+++ Easy/searchInsert.go
@@ -1,31 +1,24 @@
 package main
 
-import "fmt"
-
 func searchInsert(nums []int, target int) int {
-    l := nums[0]
-    r := nums[len(nums)-1]
-    mid := 0
-   
-
-   for l < r {
-    mid = (l + r)/2
-    if nums[mid] == target {
-        return mid
-    } else if nums[mid] < target {
-        l = mid + 1
-    } else if nums[mid] > target {
-        r = mid
-    } 
-   }
-
-    fmt.Println("l", l, "r", r)
-    fmt.Println("mid", mid)
-    return mid
+	l := 0
+	r := len(nums) - 1
+	mid := 0
+	for l <= r {
+		mid = (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		}
+	}
+	return l
 }
 
-func main() {
-    fmt.Println(searchInsert([]int{1,3,5,6}, 5)) // 2
-   // fmt.Println(searchInsert([]int{1,3,5,6}, 2)) // 1
-    //fmt.Println(searchInsert([]int{1,3,5,6}, 7)) // 4
-}
+// func main() {
+//     fmt.Println(searchInsert([]int{1,3,5,6}, 5)) // 2
+//     fmt.Println(searchInsert([]int{1,3,5,6}, 2)) // 1
+//     fmt.Println(searchInsert([]int{1,3,5,6}, 7)) // 4
+// }
