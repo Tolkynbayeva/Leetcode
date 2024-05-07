3783b5c0b02dfc9cc409c626078f4ff55ffec2de
Tolkynbayeva <workfortime37@gmail.com>
2024-05-07 22:48:31 +0500

    206. Reverse Linked List

--- Easy/reverseList.go
+++ Easy/reverseList.go
@@ -1,17 +1,8 @@
 package main
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-
 func reverseList(head *ListNode) *ListNode {
 	var prev *ListNode
 	curr := head
-
 	for curr != nil {
 		next := curr.Next
 		curr.Next = prev
