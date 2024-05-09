d8bb1c6eef7e73221afb83cf11ae6ad862de6dcc
Tolkynbayeva <workfortime37@gmail.com>
2024-05-09 23:04:56 +0500

    102. Binary Tree Level Order Traversal

---
+++ Medium/levelOrder.go
@@ -0,0 +1,25 @@
+package main
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	var result [][]int
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		var level []int
+		for i := len(queue); i > 0; i-- {
+			node := queue[0]
+			queue = queue[1:]
+			level = append(level, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		result = append(result, level)
+	}
+	return result
+}
