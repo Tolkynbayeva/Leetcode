0e92e2964fadd9e1eca3c2e928ac3e9f41d48979
Tolkynbayeva <workfortime37@gmail.com>
2024-12-30 01:48:07 +0500

    Add solution for "977. Squares of a Sorted Array"
    Implemented the `sortedSquares` function to compute the squares of a sorted array and return them in sorted order. Added a README with problem description, examples, and constraints.

---
+++ Medium/15/threeSum.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	result := [][]int{}
+
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		l := i + 1
+		r := len(nums) - 1
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+			if sum == 0 {
+				result = append(result, []int{nums[i], nums[l], nums[r]})
+				l++
+				r--
+				for l < r && nums[l] == nums[l-1] {
+					l++
+				}
+				for l < r && nums[r] == nums[r+1] {
+					r--
+				}
+			} else if sum < 0 {
+				l++
+			} else {
+				r--
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threeSum(nums))
+}
